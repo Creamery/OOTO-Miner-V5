Working but progress bar computation has a bug

CrossProcess_support.py
@@ -16,10 +16,8 @@ Applies necessary functions to output a printable chi-square comparison table.
 '''
 def crossProcess(df_dataset, np_CROSS, controller):
     key = UICS.KEY_PRE_CROSS_MODULE  # Key for progress bar
-    i_key = 1  # Iterator for progress bar
 
-    controller.updateModuleProgress(key, i_key, "Starting CROSS PROCESS MODULE")  # 1
-    i_key = i_key + 1
+    controller.updateModuleProgress(key, "Starting CROSS PROCESS MODULE")  # 1
     time.sleep(0.01)
 
     # Generate datasets as dictated by filters
@@ -28,14 +26,12 @@ def crossProcess(df_dataset, np_CROSS, controller):
     #   np_dataset_pairs[type][level]               - A list of levels within the list of cross types
     #   np_dataset_pairs[type][level][0]            - A list of dataset pairs (list) within the list of levels
     #   np_dataset_pairs[0][0][0][0]                - The contents of the list containing the dataset pairs
-    controller.updateModuleProgress(key, i_key, "Extracting Datasets by Filter")  # 2
-    i_key = i_key + 1
+    controller.updateModuleProgress(key, "Extracting Datasets by Filter")  # 2
     time.sleep(0.01)
 
     np_cross_datasets, np_cross_filters = extractDatasets(df_dataset, np_CROSS)  # TODO (Future) Try to optimize
 
-    controller.updateModuleProgress(key, i_key, "Successfully Extracted Datasets")  # 3
-    i_key = i_key + 1
+    controller.updateModuleProgress(key, "Successfully Extracted Datasets")  # 3
     time.sleep(0.01)
 
     len_cross_datasets = int(UICS.MAX_CROSS)  # len(np_cross_datasets)
@@ -47,21 +43,21 @@ def crossProcess(df_dataset, np_CROSS, controller):
 
     print("Processing - Please Wait... (Average Runtime for ALL Features - 8 minutes")
 
-    controller.updateModuleProgress(key, i_key, "Starting Cross Process : This might take some time...")  # 4
+    controller.updateModuleProgress(key, "Starting Cross Process : This might take some time...")  # 4
     time.sleep(0.01)
 
 
     # Prepare to update progress bar with the second half of the CROSS PROCESS MODULE
     key = UICS.KEY_CROSS_MODULE  # Key for progress bar
-    i_key = 1  # Iterator for progress bar
 
     # Compute the total process of this section according to the computed cross type and level count
-    UICS.CROSS_MAX_PROCESS_COUNT = computeMaxProcessCount(np_cross_datasets, len_cross_datasets, len_cross_types)
-    # Multiply by 3 since you will record each pass once, then update twice more; One for Chi-square
-    # and the other for exporting the table
-    UICS.CROSS_MAX_PROCESS_COUNT = UICS.CROSS_MAX_PROCESS_COUNT * 3
-    UICS.CROSS_MAX_PROCESS_COUNT = UICS.CROSS_MAX_PROCESS_COUNT + i_key
-    print("CPM Count " + str(UICS.CROSS_MAX_PROCESS_COUNT / 3))
+    # Compute for one pass at Level (See line commented with "LVL Pass 1")
+    UICS.CROSS_MAX_PROCESS_COUNT = computeMaxCrossLevelCount(np_cross_datasets, len_cross_datasets, len_cross_types)
+    # Multiply by 2 since you will record each pass (1) then update for exporting the table (1)
+    data_filter_process_count = computeMaxProcessCount(np_cross_datasets, len_cross_datasets, len_cross_types)
+    data_filter_process_count = data_filter_process_count  # * 2
+    UICS.CROSS_MAX_PROCESS_COUNT = UICS.CROSS_MAX_PROCESS_COUNT + data_filter_process_count
+    print("CPM Count " + str(UICS.CROSS_MAX_PROCESS_COUNT))
 
     start_time = time.time()
     # Apply Chi-square on all dataset pairs in the list np_dataset_pairs
@@ -78,32 +74,32 @@ def crossProcess(df_dataset, np_CROSS, controller):
             list_level_ssfs = []
             list_all_ssfs = []
             list_ssfs = []
+
+            # Title for the current cross process
+            str_title = "Processing CROSS[" + str(i_cross_type) + "][" + str(i_cross_level + 1) + "]"  # LVL Pass 1
+            # Update the progress bar about the current CROSS[type][level]
+            controller.updateModuleProgress(key, str_title)  # Pass 1
+            time.sleep(0.01)
+            i_process_count = 0  # Process count for current CROSS[type][level]
             # np_level_ssfs = np.array(list_level_ssfs)
             for i_dataset_pairs in range(len_cross_level):
                 dataset_pairs = cross_level[i_dataset_pairs]
                 len_dataset_pairs = len(dataset_pairs)
 
-                # Title for the current cross process
-                str_title = "Processing CROSS[" + str(i_cross_type) + "][" + str(i_cross_level) + "]"
-                # Update the progress bar about the current CROSS[type][level]
-                controller.updateModuleProgress(key, i_key, str_title)
-                i_key = i_key + 1
-                time.sleep(0.01)
+                str_cross_level_length = str(len_cross_level)
+                #  Description for the current cross process
+                str_description = "     " + str(i_dataset_pairs) + " of " + str_cross_level_length
+                controller.updateModuleProgress(key, str_description)  # INNER PASS 1
 
-                str_dataset_length = str(len_dataset_pairs)
                 for i_dataset_pair in range(len_dataset_pairs):
-                    #  Description for the current cross process
-                    str_description = "     " + str(i_dataset_pair) + " of " + str_dataset_length
-                    controller.updateModuleProgress(key, i_key, str_description)
-                    i_key = i_key + 1
 
                     dataset_pair = dataset_pairs[i_dataset_pair]
 
                     dict_chi_square = CHIS.chiSquare(dataset_pair)
 
-                    controller.updateModuleProgress(key, i_key, "Applying Chi-square")
-                    i_key = i_key + 1
+                    # controller.updateModuleProgress(key, "Applying Chi-square")
                     # time.sleep(0.01)
+
                     df_processed_output, list_ssf, list_sig_output = CHIS.processChiSquareTable(dict_chi_square)
 
                     if df_processed_output is not None:
@@ -119,8 +115,7 @@ def crossProcess(df_dataset, np_CROSS, controller):
                         # list_chi_square_output.append([df_output, np_dataset_pair_filter])
                         list_index = [i_cross_type, i_cross_level]
 
-                        controller.updateModuleProgress(key, i_key, "Exporting Chi-square Table")
-                        i_key = i_key + 1
+                        # controller.updateModuleProgress(key, "Exporting Chi-square Table")
                         # time.sleep(0.01)
                         # TODO Printing
                         df_output, str_pair_name = LS.exportChiSquareTable(df_processed_output,
@@ -128,8 +123,9 @@ def crossProcess(df_dataset, np_CROSS, controller):
                                                                            list_index)
 
                         dict_result_table_sig = addToDictionaryResult(dict_result_table_sig, str_pair_name, list_sig_output)
-                    else:
-                        i_key = i_key + 2  # Add 2 to make up for the missed processes
+                    # else:
+                        # controller.updateModuleProgress(key, str_description)  # Pass 2
+                        # Add 1 to make up for the missed processes
                         # print("DF OUTPUT IS NULL: Skipping Item")
 
 
@@ -165,6 +161,21 @@ def computeMaxProcessCount(np_cross_datasets, len_cross_datasets, len_cross_type
                 process_count = process_count + 1  # Count the number of dataset pairs
 
     return process_count
+
+def computeMaxCrossLevelCount(np_cross_datasets, len_cross_datasets, len_cross_types):
+    process_count = 0
+
+    for i_cross_type in range(len_cross_datasets):
+        cross_type = np_cross_datasets[i_cross_type]  # Iterate through each CROSS TYPE
+
+        for i_cross_level in range(len_cross_types):
+            cross_level = cross_type[i_cross_level]  # Iterate through each LEVEL
+
+            process_count = process_count + 1  # Count the number of pass per levels
+
+    return process_count
+
+
 
 def mergeUnique(list1, list2):
     # in_first = set(list1)